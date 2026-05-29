b008cb64dfadd9aad0bee2fe314636c5fd080d4f
ashray <ashray.saxena@lat.com>
2026-05-29 18:59:57 +0530

    Create duplicate.cpp

---
+++ 001/duplicate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printArray(int arr[], int size){
+  for (int i = 0 ; i < size ; i++){
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int findDuplicate(int arr[], int size){
+  int ans = 0;
+  for (int i = 0; i < size; i++){
+    ans = ans ^ arr[i];
+  }
+  return ans;
+}
+
+int main(){
+  int size;
+  int arr[10];
+  cin >> size;
+  for (int i = 0; i<size; i++){
+    cin >> arr[i];
+  }
+  cout << printaArray(arr, size);
+  cout << findDuplicate(arr, size);
+}
