f0d15a449fd36a2c2cee3c0a9901a3bd754bc0c1
ashray <ashray.saxena@lat.com>
2026-05-29 16:08:01 +0530

    Create dectobin to dec.cpp

---
+++ 001/dectobin to dec.cpp
@@ -0,0 +1,60 @@
+/*
+#include <iostream>
+using namespace std;
+
+int pow(int n, int p) {
+    return (p==0) ? 1 : n*pow(n, p-1);
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+
+    int answer = 0;
+    int i = 0;
+
+    while(n != 0) {
+        int bit = n & 1;
+
+        answer = answer + (bit * pow(10,i));
+
+        i++;
+        n = n >> 1;
+    }
+
+    cout<<"Converted Binary Number : "<<answer<<endl;
+
+    return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+int pow(int n, int p) {
+    return (p==0) ? 1 : n*pow(n, p-1);
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+
+    int answer = 0;
+    int i = 0;
+
+    while(n != 0) {
+        int digit = n % 10;
+
+        if(digit == 1) {
+            answer = answer + pow(2,i);
+        }
+
+        i++;
+        n /= 10;
+    }
+
+    cout<<"Converted Binary Number : "<<answer<<endl;
+
+    return 0;
+}
