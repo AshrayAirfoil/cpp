a8e99412cc006e3095bc7b664ff4bdda5ed1c2bc
ashray <ashray.saxena@lat.com>
2026-05-29 13:44:22 +0530

    Create introduction.cpp

---
+++ 001/introduction.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+  cout << "Hello World" << "\n";
+  int a = 'a';
+  cout << a << "\n";
+  char ch1 = 123456;
+  cout << ch1 << "\n"
+
+
+  
+}
+
